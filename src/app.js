bf2ebe2e7796d99e562c53cb0bcf19ed75c32284
tulin-subasi <tulinsubasi@gmail.com>
2022-08-10 20:02:26 +0300

    api problem solved

--- src/app.js
+++ src/app.js
@@ -31,7 +31,8 @@ day.innerHTML = `${currentDay} ${currentHour}:${currentMinutes}`;
 /*
 Add Weather Forecast Function
 */
-function displayForecast() {
+function displayForecast(response) {
+  console.log(response.data.daily);
   let forecastElement = document.querySelector("#forecast");
   let forecastHTML = `<div class="row justify-content-start">`;
   let forecastDay = ["Fri", "Sat", "Sun"];
@@ -60,17 +61,18 @@ function displayForecast() {
   forecastHTML = forecastHTML + `</div>`;
   forecastElement.innerHTML = forecastHTML;
 }
-displayForecast();
+
 /*
 🕵️‍♀️Feature #2
 Add a search engine, when searching for a city (i.e. Paris), display the city name on the page after the user submits the form.
 */
 
 function getForecast(coordinates) {
-  console.log(coordinates);
+  //console.log(coordinates);
   let apiKey = "29adcafc0b9fced5934f93a3b452d5af";
-  let apiUrl = `https://api.openweathermap.org/data/3.0/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
-  console.log(apiUrl);
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+  //console.log(apiUrl);
+  axios.get(apiUrl).then(displayForecast);
 }
 
 let frm = document.querySelector("#form-search");
